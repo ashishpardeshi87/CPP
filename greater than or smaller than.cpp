3afa4034e9fe4e7d2bd571af8f6da232f8702962
Ashish Pardeshi <143716256+ashishpardeshi87@users.noreply.github.com>
2024-08-08 12:27:11 +0530

    greater than or smaller than
    The program prompts the user to enter two numbers. It then compares these numbers and prints whether the first number is greater than, smaller than, or equal to the second number.

---
+++ greater than or smaller than.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{ 
+  int number = 3;
+  // Check if the number is greater than 5
+  if(number>5) 
+  {
+    cout <<"The number is greater than 5."<< endl;
+  }
+  else
+  {
+    cout <<"The number is 5 or less."<< endl;
+  }
+  return 0;
+}
+ 
+  
+ 

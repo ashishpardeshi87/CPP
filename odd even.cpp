7ee463f6d70cefc26427bf755633ec9f84aec805
Ashish Pardeshi <143716256+ashishpardeshi87@users.noreply.github.com>
2024-08-08 12:24:04 +0530

    odd even number
    The program prompts the user to enter an integer. It then checks the integer to see if it is even or odd by using the modulus operator %. If the integer modulo 2 is 0, the number is even; otherwise, it is odd.

---
+++ odd even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a=10;
+  cout<<"Enter the number:";
+  if(10%2==0){
+    cout<<"The given number is EVEN"<<endl;
+  }else{
+    cout<<"The given number is ODD"<<endl;
+  }
+  return 0;
+  
+  
+}
+  

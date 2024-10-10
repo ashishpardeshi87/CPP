d3fd537816a200943e6e84b6060d15297a9ca840
Ashish Pardeshi <143716256+ashishpardeshi87@users.noreply.github.com>
2024-10-10 09:48:57 +0530

    Add files via upload

---
+++ Destructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Test{
+  public:
+  //User-Defined Constructor
+  Test(){cout<<"\n Constructor executed";}
+  
+  //User-Defined Destructor
+  ~Test(){cout<<"\nDestructor executed";}
+  
+};
+main()
+{
+  Test t;
+  
+  return 0;
+  
+}
+
+Output:
+
+
+ Constructor executed
+Destructor executed

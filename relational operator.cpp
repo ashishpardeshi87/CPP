8809931c60016ea8e4c8c8d926e33702ed813b5a
Ashish Pardeshi <143716256+ashishpardeshi87@users.noreply.github.com>
2024-10-10 09:51:33 +0530

    Add files via upload

---
+++ relational operator.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a=6,b=4;
+  
+  //Equal to operator
+  cout<<"a == b is "<<(a == b)<<endl;
+  
+  //Greater than operator
+  cout<<"a > b is "<<(a > b)<<endl;
+  
+  //Greater than or Equal to operator
+  cout<<"a >= b is "<<(a >= b)<<endl;
+  
+  //Lesser than operator
+  cout<<"a < b is "<<(a < b)<<endl;
+  
+  //Lesser than or Equal to operator
+  cout<<"a <= b is "<<(a <= b)<<endl;
+  
+  //true
+  cout<<"a != b is "<<(a != b)<<endl;
+  
+  return 0;
+  
+  //Here 0 denotes false and 1 denotes true
+  
+}
